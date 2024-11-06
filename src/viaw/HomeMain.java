3e6b5115ff376cc030939f5e77abc131b72c3af3
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-11-06 16:59:48 -0300

    implementação do menu, principal do projeto Imobiliaria.

--- src/viaw/HomeMain.java
+++ src/viaw/HomeMain.java
@@ -1,5 +1,24 @@
+/***Classe Principal para Testar o Sistema**
+
+→ Crie uma classe principal com o método main para testar o sistema da imobiliária.
+
+→ Crie um objeto do tipo Imobiliaria, adicione diferentes construções ao catálogo, liste as construções disponíveis e informe a área total das construções no catálogo.
+*/
+
 package viaw;
+
+import controller.Imobiliaria;
 
 public class HomeMain {
 
+	public static void main(String[] args) {
+		Imobiliaria i = new Imobiliaria();
+		
+		i.adicionarConstrucao(null);
+		
+		i.removerConstrucao(null);
+		
+		i.listarConstrucaes();
+		
+	}
 }
